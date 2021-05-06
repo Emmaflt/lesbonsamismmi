d1c9b1322c58330866ea401b661d70316af5b470
Emmaflt <emmafouillat@gmail.com>
2021-05-06 16:53:28 +0200

    deuxieme version

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :offers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "articles#index"
+  root "offers#index"
   
   get "/articles", to: "articles#index"
 
